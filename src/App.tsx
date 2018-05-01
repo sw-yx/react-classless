1659667120475f178ffc3e94c3c47e4abcd6a35e
tsiq-swyx <shawn.wang@twosigmaiq.com>
2018-04-30 21:28:08 -0400

    dcomponent has weird state issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,70 @@
 import * as React from 'react';
 import './App.css';
 
-import { Component, SetState } from './components/index';
+import { Component, DComponent, SetState } from './components/index';
 
 /* tslint:disable:jsx-no-lambda */
 class App extends React.Component {
+  public state = {
+    text: 'beforeclick'
+  };
+  public handleClick = () => this.setState({ text: 'afterclick' });
   public render() {
     return (
       <div className="App">
         <header className="App-header">
-          <Component
-            initialState={{ test: 'hi' }}
-            didMount={({ setState }) => setTimeout(() => setState({ test: 'typescript react' }), 3000)}
-          >
-            {({ state }) => <div> hello {state.test}</div>}
-          </Component>
+          {/* <button onClick={this.handleClick}>click me</button> */}
+          <TestComponent testprop={this.state.text} />
         </header>
       </div>
     );
   }
+}
+// function TestComponent(props: { testprop: string }) {
+//   return (
+//     <Component
+//       initialState={{ test: 'hi' }}
+//       didMount={({ setState }) => setTimeout(() => setState({ test: 'typescript react' }), 1000)}
+//       didUpdate={({ snapshot }) => console.warn({ snapshot })} // tslint:disable-line
+//       beforeUpdate={() => `hai i'm a ${props.testprop}`}
+//     >
+//       {({ state }) => (
+//         <div>
+//           <div>hello {state.test}</div>
+//         </div>
+//       )}
+//     </Component>
+//   );
+// }
+
+function TestComponent(props: any) {
+  function didMount({ setState }) {
+    /* tslint:disable */
+    setTimeout(function() {
+      setState({ test: 'typescript react' });
+    }, 3000);
+  }
+  function render({ state, setState }) {
+    return (
+      <div>
+        <button onClick={() => setState({ test: 'bye' })}>click me</button>
+        <div>hello {state.test}</div>
+        <div>derived: {state.deriv}</div>
+      </div>
+    );
+  }
+  return (
+    <DComponent
+      initialState={{ test: 'hi' }}
+      // didMount={({ setState }) => setTimeout(() => setState({ test: 'typescript react' }), 3000)}
+      didMount={didMount}
+      derivedState={(_, prevState) => ({ deriv: prevState.test + '123' })}
+      didUpdate={({ snapshot }) => console.warn({ snapshot })} // tslint:disable-line
+      beforeUpdate={({ prevState }) => `hai i'm a ${prevState.test}`}
+    >
+      {render}
+    </DComponent>
+  );
 }
 
 export default App;
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,19 +1,20 @@
 import * as React from 'react';
 
-/* tslint:disable:interface-name variable-name jsdoc-format */
+/* tslint:disable:interface-name variable-name jsdoc-format max-classes-per-file*/
 
 export type SetState = ({}, a?: () => {}) => {} | void;
 export interface PropsAndState {
-  props: object;
+  props: {};
   state: any;
 }
 export interface DidUpdateProps extends RProps {
-  prevProps: object;
-  prevState: object;
+  prevProps: any;
+  prevState: {};
+  snapshot?: {};
 }
 export interface ShouldUpdateProps extends PropsAndState {
-  nextProps: object;
-  nextState: object;
+  nextProps: {};
+  nextState: {};
 }
 export interface RProps extends PropsAndState {
   forceUpdate: (a?: {}) => {} | void;
@@ -24,17 +25,62 @@ export interface Props {
   /**
    * Message to append to component
    */
-  initialState?: object;
+  initialState?: {};
   didMount?: (props?: RProps) => {};
-  didUpdate?: (props?: DidUpdateProps) => {};
+  beforeUpdate?: (props?: DidUpdateProps) => {};
+  didUpdate?: (props?: DidUpdateProps) => void;
   shouldUpdate?: (props?: ShouldUpdateProps) => boolean;
   willUnmount?: (props?: PropsAndState) => {};
-  render?: (props?: RProps) => React.ReactNode;
+  render?: (props?: RProps) => React.ReactNode | undefined;
   children?: (props?: RProps) => React.ReactNode;
+
+  // passthrough
+  // setState?: any;
+  // forceUpdate?: any;
+  // new lifecycles!
+  derived?: {};
+}
+
+// experimental thing to wrap getDerivedStateFromProps... need to actually use it
+export function DComponent(props: any) {
+  class Instance extends React.Component {
+    public static getDerivedStateFromProps(nextProps: {}, prevState: {}) {
+      if (props.derivedState) {
+        return props.derivedState(nextProps, prevState);
+      }
+      return null;
+    }
+    public state = props.initialState;
+    public render() {
+      // return <Component {...this.props} derived={this.state} />;
+      const { children = () => 'hi' }: { children?: (props?: RProps) => React.ReactNode } = this.props;
+      return (
+        <Component
+          initialState={props.initialState}
+          didMount={props.didMount}
+          beforeUpdate={props.beforeUpdate}
+          didUpdate={props.didUpdate}
+          shouldUpdate={props.shouldUpdate}
+          willUnmount={props.willUnmount}
+          render={children && children.bind(this)} // tslint:disable-line
+          derived={this.state}
+        />
+      );
+    }
+    // public render() {
+    //   const { children, ...rest }: { children: (props?: RProps) => React.ReactNode } & any = this.props;
+    //   return (
+    //     <Component {...rest} derived={this.state}>
+    //       {args => children(args)}
+    //     </Component>
+    //   );
+    // }
+  }
+  return <Instance {...props} />;
 }
 
 export class Component extends React.Component<Props> {
-  public state: object = this.props.initialState || {};
+  public state: {} = this.props.initialState || {};
 
   public componentDidMount() {
     if (this.props.didMount) {
@@ -42,7 +88,7 @@ export class Component extends React.Component<Props> {
     }
   }
 
-  public shouldComponentUpdate(nextProps: object, nextState: object) {
+  public shouldComponentUpdate(nextProps: {}, nextState: {}) {
     if (this.props.shouldUpdate) {
       return this.props.shouldUpdate({
         nextProps,
@@ -51,6 +97,7 @@ export class Component extends React.Component<Props> {
         state: this.state
       });
     } else {
+      console.log('scu true'); // tslint:disable-line
       return true;
     }
   }
@@ -64,15 +111,28 @@ export class Component extends React.Component<Props> {
     }
   }
 
-  public componentDidUpdate(prevProps: object, prevState: object) {
+  public componentDidUpdate(prevProps: {}, prevState: {}, snapshot: {}) {
     if (this.props.didUpdate) {
       this.props.didUpdate(
+        Object.assign(this.getArgs(), {
+          prevProps,
+          prevState,
+          snapshot
+        })
+      );
+    }
+  }
+
+  public getSnapshotBeforeUpdate(prevProps: {}, prevState: {}) {
+    if (this.props.beforeUpdate) {
+      return this.props.beforeUpdate(
         Object.assign(this.getArgs(), {
           prevProps,
           prevState
         })
       );
     }
+    return null;
   }
 
   public render() {
@@ -86,6 +146,8 @@ export class Component extends React.Component<Props> {
         : null;
   }
 
+  // private _setState =  (updater: {}, cb?: () => {}) => this.props.setState(updater, cb) || this.setState(updater, cb);
+  // private _forceUpdate = (cb?: () => {}) => this.props.forceUpdate(cb) || this.forceUpdate(cb);
   private _setState = (updater: {}, cb?: () => {}) => this.setState(updater, cb);
   private _forceUpdate = (cb?: () => {}) => this.forceUpdate(cb);
 
@@ -95,9 +157,9 @@ export class Component extends React.Component<Props> {
       forceUpdate,
       props,
       setState,
-      state
+      state: { ...state, ...props.derived }
     };
   }
 }
 
-// private _setState = (...args: object[]) => this.setState(...args);
+// private _setState = (...args: {}[]) => this.setState(...args);
